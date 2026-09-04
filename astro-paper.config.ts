0094e8bfb3a49983304317c028d7d64b2fce7f7c
Mehdy Akbarzadeh <mehdy.arbatan@gmail.com>
2026-09-04 22:06:44 +0300

    Changed the Title

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -3,9 +3,9 @@ import { defineAstroPaperConfig } from "./src/types/config";
 export default defineAstroPaperConfig({
   site: {
     url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
+    title: "Mehdi Akbarzadeh | Plant Design Automation",
+    description: "Plant Design Automation Developer & Mechanical/Piping Engineer",
+    author: "Mehdi Akbarzadeh",
     profile: "https://satna.ing",
     ogImage: "default-og.jpg",
     lang: "en",
